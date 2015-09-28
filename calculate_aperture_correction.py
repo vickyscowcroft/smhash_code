corrected treatment of masked array in aperture correction calculation

calculate_aperture_correction.py
@@ -8,6 +8,7 @@ from astropy.stats import sigma_clip
 ## Updated 28/09/2015 to use sigmaclipping to remove bad points rather than relying on a hard wired magnitude range
 
 input = sys.argv[1]
+sigma = float(sys.argv[2])
 
 apc, eapc, alf, ealf = np.loadtxt(input, skiprows=3, usecols=(3, 4, 5, 6), unpack='TRUE')
 
@@ -21,10 +22,10 @@ difference  = apc - alf
 
 ## Using astropy.stats.sigma_clip
 ## This version returns a masked array rather than a cut array
-clipped = sigma_clip(difference, sig = 2.5, iters=100)
+clipped = sigma_clip(difference, sig = sigma, iters=100)
 
-av_diff = np.average(clipped)
-sdev_diff = np.std(clipped)
+av_diff = np.ma.mean(clipped)
+sdev_diff = np.ma.std(clipped)
 
 
 total_err = np.sqrt(eapc**2 + ealf**2)
@@ -35,6 +36,9 @@ axp1 = mp.subplot(111)
 #axp1.plot(apc, difference, 'k.', ls='none')
 #axp1.axhline(av_diff, color='r', ls='--')
 
+
+
+axp1.errorbar(apc, clipped, yerr = total_err, color='grey', ls='none')
 axp1.plot(apc, clipped, 'k.', ls='none')
 axp1.axhline(av_diff, color='r', ls='--')
 axp1.axhline(av_diff+2*sdev_diff, color='b', ls='--')