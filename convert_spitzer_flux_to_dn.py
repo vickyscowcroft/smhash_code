d315ba7c5414dc039fc4d72ebbcf65aec6c7de7e
Vicky Scowcroft <vickyscowcroft@gmail.com>
2015-09-29 16:20:54 -0400

    function for spitzer image conversion

--- convert_spitzer_flux_to_dn.py
+++ convert_spitzer_flux_to_dn.py
@@ -9,7 +9,8 @@ def spitzer_flux2dn(image, newname=""):
 		newname = re.sub(".fits", "_dn.fits", image)
 	print newname
 	shutil.copy(image, newname)
-	hdulist = fits.open(newname)
+	hdulist = fits.open(newname, mode='update')
+#	hdulist.fileinfo()
 	prihdr = hdulist[0].header
 	scidata = hdulist[0].data
 	exptime = prihdr['exptime']
@@ -17,5 +18,21 @@ def spitzer_flux2dn(image, newname=""):
 	scidata *= exptime / fluxconv
 	return(0)
 	
+def spitzer_flux2dn_mos(image, median, new_target_stem, channel):
+	newname = new_target_stem + "_" + channel + "_dn.fits"
+	shutil.copy(image, newname)
+	hdulist = fits.open(newname, mode='update')
+	prihdr = hdulist[0].header
+	scidata = hdulist[0].data
+	exptime = prihdr['exptime']
+	fluxconv = prihdr['fluxconv']
+	scidata *= exptime / fluxconv
+	newmosname = new_target_stem + "_" + channel + "_med_dn.fits"
+	shutil.copy(median, newmosname)
+	hdulist = fits.open(newmosname, mode='update')
+	scidata = hdulist[0].data
+	scidata *= exptime / fluxconv
+	return(0)
 	
+		
 	
