e2e9bb7a4ad797d029f20f71238d3d95e9622c87
Victoria Scowcroft <vickyscowcroft@gmail.com>
2016-07-01 09:37:32 +0100

    adding back full phot pipeline

---
+++ full_photometry_pipeline/sagittarius_pipeline.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python
+
+
+import sys
+import glob
+import sgr_setup
+import sgr_initial_phot
+import sgr_pre_allframe_matching
+import sgr_median_image_phot
+import shutil
+import re
+import pexpect
+import aperture_correction_photometry
+import calculate_aperture_correction
+import apply_aperture_correction
+import calibrate_all_epochs
+import all_location_corrections
+import os
+
+
+target_name = sys.argv[1]
+location = sys.argv[2] ## location is the location of the RRL - can currently be orphan, sgr_stream
+
+
+if(location=='orphan'):
+	cat_target = target_name
+	cat_name = '/Users/vs/Dropbox/SMHASH/orphan_rrlyrae_catalogue'
+if(location=='sgr_stream'):
+	cat_target = target_name[0:11] + '.' + target_name[12:20]
+	cat_name = '/Users/vs/Dropbox/SMHASH/catalina_sagittarius_rrlyrae_catalogue'
+
+## Convert the images to counts and give them sensible names
+## Also cleans up old runs
+target_stem = sgr_setup.sgr_setup(target_name)
+
+file_list = glob.glob(target_stem + '_e*_3p6um_dn.fits')
+
+#sgr_setup.bad_pixel_mask(file_list)
+
+## Do initial aperture photometry on all the science images
+
+for image in file_list:
+	print image
+	sgr_initial_phot.init_aper_phot(image)
+	
+## Haven't automated this part yet. 
+## You have to go to IRAF, select good PSF stars, make a daophot PSF, check the allstar subtraction and come back to this window
+## Keep the name of the .psf file as the default
+print "Now make a good PSF for epoch 1 by hand"
+print "Select stars visually, choosing them from the .coo file and copying their info to the .lst file"
+print "Check the subtracted image for epoch 1"
+print "IMPORTANT!! If you've copied a psf from somewhere, you still need to run allstar on epoch 1"
+print "VS will fix this `feature' in a later version"
+print "When you're done, type continue"
+
+psfdone = raw_input('Type continue when PSF is complete: ')
+
+if (psfdone == 'continue'):
+	print "Continuing..."
+
+else:
+	print "Have you made a good PSF?"
+	psfdone = raw_input('Type continue when PSF is complete: ')	
+	
+## Now copy the psf to each epoch
+shutil.copy(target_stem + '_e1_3p6um_dn.psf', 'master_3p6um.psf')
+for file in file_list:
+	psfname = re.sub(".fits",".psf", file)
+	shutil.copy("master_3p6um.psf", psfname)
+	
+## Prepare the files for allframe
+
+sgr_pre_allframe_matching.sgr_pre_allframe_matching(target_stem, '1')
+print 'completed pre allframe matching?'
+
+## Do the photometry on the median image and calculate it's offset from epoch 1
+
+median_name = target_stem + '_3p6um_med_dn.fits'
+print 'median name is ', median_name
+
+sgr_median_image_phot.sgr_median_image_phot(median_name)
+
+## ALLFRAEME
+
+## Remove old files from possible previous allframe runs to prevent restarting crashed run
+if os.path.isfile(target_stem + '_3p6m.bck'): os.remove(target_stem + '_3p6um.bck')
+if os.path.isfile(target_stem + '_3p6um.tfr'): os.remove(target_stem + '_3p6um.tfr')
+
+print "running allframe"
+
+allframe = pexpect.spawn("allframe")
+allframe.logfile = sys.stdout
+allframe.expect("OPT>")
+allframe.sendline("")
+allframe.expect("File with list of images")
+allframe.sendline(target_stem + '_3p6um.mch')
+allframe.expect("File with list of stars")
+allframe.sendline(target_stem + '_3p6um.mag')
+allframe.expect("Starting image")
+allframe.expect("Good bye", timeout=6000)
+allframe.close()
+print allframe.isalive()
+
+print "ALLFRAME complete"
+
+## Begin calibration process
+
+## Aperture correction
+
+flux_epoch_1 = target_stem + '_e1_3p6um'
+
+aperture_correction_photometry.apcor_photo(flux_epoch_1)
+
+apcor, sdev_apcor, period = calculate_aperture_correction.calc_apcor(flux_epoch_1, 'apcor.raw', 2.0, cat_target, cat_name)
+
+apply_aperture_correction.apply_apcor(target_stem + '_e1_3p6um_dn.alf', apcor, 1)
+
+## Calibrate all other epochs
+calibrate_all_epochs.calibrate(target_stem + '_3p6um.mch')
+
+## location correction
+all_location_corrections.location_corr(target_stem + '_3p6um.mch')
+
+print "Calibration complete"
+
+print "Individual epochs are in " + target_stem + "_epoch_3p6um_dn.cal files"
+print "Matched file is " + target_stem + '_3p6um.cal'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ full_photometry_pipeline/sgr_initial_phot.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+
+
+import numpy as np
+import re
+import sys
+import pexpect ## Use this to control daophot
+import shutil
+from astropy.io import fits
+import os
+
+def init_aper_phot(fitsfile):
+
+	image = re.sub(".fits", "", fitsfile)
+
+## Copy the daophot opt file
+	shutil.copy("/Users/vs/Dropbox/Python/smhash_code/daophot-spitzer-i1.opt", "daophot.opt")
+	shutil.copy("/Users/vs/Dropbox/Python/smhash_code/photo-spitzer.opt", "photo.opt")
+
+	print "Working on " + image
+## Clean up previous runs
+
+	extensions = ['.coo', '.lst', '.psf', '.nei', '.ap', '.als', 's.coo', 's.ap', '.srt', '.cmb', 's.fits', '.als']
+	for ext in extensions:
+		if (os.path.isfile(image + ext)):
+			os.remove(image+ext)
+
+## Running daophot
+
+	daophot = pexpect.spawn("daophot")
+
+	hdulist = fits.open(fitsfile)
+	prihdr = hdulist[0].header
+	n_bcds = prihdr['totalbcd'] ### Getting the correct number of BCDs that went into the stack
+	#if (n_bcds > 20): #### Changing the threshold for the long exposure frames. Change it to do this a smarter way eventually
+	#	daophot.expect("Command:")
+	#	daophot.sendline("opt")
+	#	daophot.expect("INPUT\):")
+	#	daophot.sendline("")
+	#	daophot.expect("OPT>")
+	#	daophot.sendline("th=2")
+	#	daophot.expect("OPT>")
+	#	daophot.sendline("")
+# attach the image
+	daophot.expect("Command:")
+	daophot.sendline("at " + image)
+
+# find the stars
+	daophot.expect("Command:")
+	daophot.sendline("find")
+	daophot.expect("Number of frames averaged, summed:")
+	daophot.sendline("1," + str(np.sqrt(n_bcds)))
+	daophot.expect("File for positions")
+	daophot.sendline("")
+	daophot.expect("Are you happy with this?")
+	daophot.sendline("y")
+
+	print "FIND complete"
+
+## Aperture photometry
+	daophot.expect("Command:")
+	daophot.sendline("phot")
+	daophot.expect("File with aperture radii")
+	daophot.sendline("")
+	daophot.expect("PHO>")
+	daophot.sendline("")
+	daophot.expect("Input position file")
+	daophot.sendline(image + '.coo')
+	daophot.expect("Output file")
+	daophot.sendline(image + '.ap')
+
+	print "PHOT complete"
+
+## Exit daophot
+	daophot.expect("Command:")
+	daophot.sendline("exit")
+	daophot.close(force=True)
+
+	print "Initial daophot run complete."
+
+
